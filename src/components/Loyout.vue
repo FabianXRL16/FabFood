13b1d7cadde926713d11046467e766ada49607c4
Fabian Pacherres <fabianpacherres@gmail.com>
2021-08-20 15:46:01 -0500

    Loyout creation

---
+++ src/components/Loyout.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        loyout
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "Loyout",
+    components:{},
+    data(){
+        return{}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
