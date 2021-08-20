1440c6f062086400717c497d37eda961351c4fc3
Fabian Pacherres <fabianpacherres@gmail.com>
2021-08-20 15:45:44 -0500

    Delete HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <loyout />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import loyout from '@/components/Loyout.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    loyout
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body{
+  margin: 0;
+  font-family: sans-serif;
 }
 </style>
