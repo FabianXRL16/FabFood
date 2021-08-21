5077a872f1fe422d5396c4222305b567266e0614
Fabian Pacherres <fabianpacherres@gmail.com>
2021-08-21 17:25:33 -0500

    Button slider creation

---
+++ src/components/inputs/buttonSlider.vue
@@ -0,0 +1,37 @@
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "buttonSlider",
+};
+</script>
+
+<style scoped>
+@import "../../assets/styles/variables.css";
+button {
+  background-color: var(--bg-primary);
+  height: 25px;
+  width:25px;
+  border-radius: 12.5px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  display: grid;
+  place-content: center;
+  transform: scale(.8);
+  transition: .3s;
+}
+
+button:hover{
+  transform: scale(1);
+  transition: .3s;
+}
+
+button img {
+  height: 12px;
+}
+</style>
