09464a4d1bc3b5d366685318baf1c32f08e33b69
Fabian Pacherres <fabianpacherres@gmail.com>
2021-08-21 03:20:31 -0500

    buttons

---
+++ src/components/inputs/buttonCar.vue
@@ -0,0 +1,40 @@
+<template>
+  <button>
+    <div class="count">
+      <strong>10+</strong>
+    </div>
+    <img src="../../assets/img/carrito.png" alt="" />
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+@import "../../assets/styles/variables.css";
+
+button {
+  background-color: transparent;
+  border: none;
+  position: relative;
+}
+
+.count {
+  position: absolute;
+  color: var(--white);
+  background-color: var(--bg-primary);
+  border-radius: 50%;
+  width: 20px;
+  height: 20px;
+  right: 0;
+  display: grid;
+  place-items: center;
+  font-size: 10px;
+  font-family: popinsLight;
+}
+
+button img {
+  height: 30px;
+}
+</style>
--- src/components/inputs/buttonPrimary.vue
+++ src/components/inputs/buttonPrimary.vue
@@ -13,3 +13,15 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+@import "../../assets/styles/variables.css";
+button{
+    background-color: var(--bg-primary);
+    border: 1px solid transparent;
+    padding: 12px 30px 12px;
+    border-radius: 22px;
+    color: var(--white);
+    font-size: var(--btn-text-primary);
+}
+</style>
