28fec16e8c2a71eb0d7ccc3fed2abbf9c6ad63a0
Fabian Pacherres <fabianpacherres@gmail.com>
2021-08-21 01:04:15 -0500

    nav creation

---
+++ src/components/general/ffnav.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        nav
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ffnav"
+}
+</script>
+
+<style scoped>
+div{
+    background-color: red;
+}
+</style>
