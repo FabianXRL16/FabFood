a0e5d0a7723573ad33a1ccbd9ced6060ee73faf3
Fabian Pacherres <fabianpacherres@gmail.com>
2021-08-22 16:13:50 -0500

    Header Fixed

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,56 @@
 <template>
   <div id="app">
-    <ffheader />
+    <ffheader class="header" />
+    <div class="space"></div>
     <router-view class="content" />
   </div>
 </template>
 
 <script>
-import ffheader from "./components/general/ffheader.vue"
+import ffheader from "./components/general/ffheader.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    ffheader
-  }
-}
+    ffheader,
+  },
+};
 </script>
 
 <style>
 @import "./assets/styles/variables.css";
-html, body{
+html,
+body {
   margin: 0;
   font-family: var(--font_primary);
   background-image: url(./assets/img/background-home.jpg);
   background-size: cover;
   background-repeat: no-repeat;
   background-position-x: center;
+  background-repeat: no-repeat;
+background-attachment: fixed;
   height: 100vh;
 }
-
-.content{
-    height: calc(100vh - 70px);
+.header {
+  position: fixed;
+  width: 100%;
+  box-sizing: border-box;
+}
+.space {
+  height: 70px;
+}
+.content {
+  height: calc(100vh - 70px);
 }
 
-@media (max-width: 500px) { 
-  html, body{
+@media (max-width: 500px) {
+  html,
+  body {
     background-image: url(./assets/img/background-home-mobile.jpg);
     background-size: cover;
     background-position-x: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
   }
 }
 </style>
