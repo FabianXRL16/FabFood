763a21b09e4af3194294e6b004cc7c06a1d471e4
Fabian Pacherres <fabianpacherres@gmail.com>
2021-08-22 22:27:40 -0500

    View FindFood creation

---
+++ src/views/FindFood.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <back :title="title" />
+    </div>
+</template>
+
+<script>
+import back from "@/components/general/back.vue"
+export default{
+    name: "FindFood",
+    components: {back},
+    data(){
+        return{
+            title: "To eat!"
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
