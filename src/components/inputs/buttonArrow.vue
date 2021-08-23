f0ed8168fbb3ad7e8af86471455183278f57249b
Fabian Pacherres <fabianpacherres@gmail.com>
2021-08-22 22:29:53 -0500

    Button arrow outline

---
+++ src/components/inputs/buttonArrow.vue
@@ -0,0 +1,79 @@
+<template>
+  <button :class="typeBtn ? 'button' : 'button_otuline'">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "buttonSlider",
+  props: {
+    typeBtn: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import "../../assets/styles/variables.css";
+.button {
+  background-color: var(--bg-primary);
+  height: 25px;
+  width: 25px;
+  border-radius: 12.5px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  display: grid;
+  place-content: center;
+  transform: scale(0.8);
+  transition: 0.3s;
+  cursor: pointer;
+}
+
+.button:hover {
+  transform: scale(1);
+  transition: 0.3s;
+}
+
+.button img {
+  height: 12px;
+}
+
+.button_otuline img {
+  height: 16px;
+}
+
+.button_otuline {
+  background: transparent;
+  cursor: pointer;
+  width: auto;
+  border: none;
+  outline: none;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+}
+
+strong {
+  font-size: 18px;
+  color: var(--white);
+}
+
+@media (max-width: 820px) {
+  strong {
+    display: none;
+  }
+  .button_otuline {
+    padding: 0;
+    width: 12.33px;
+    border: none;
+    outline: none;
+  }
+  .button_otuline img {
+    height: 20px;
+  }
+}
+</style>
