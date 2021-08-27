5eb3f57d0b5b243e4de260bc35c6fd5d7a6ccfce
Fabian Pacherres <fabianpacherres@gmail.com>
2021-08-27 15:10:21 -0500

    Remove the content in the hover that was unnecessary

--- src/components/items/category.vue
+++ src/components/items/category.vue
@@ -13,44 +13,47 @@ export default {
 
 <style scoped>
 @import "../../assets/styles/variables.css";
-.category{
-    min-width: 80px;    
-    height: 100.8px;
-    border-radius: 50px;
-    display: grid;
-    grid-auto-flow: row;
-    gap: 10px;
-    transform: scale(.9);
-    transition: .3s;
-    border: none;
-    background-color: transparent;
-    cursor: pointer;
-    outline: none;
+.category {
+  min-width: 80px;
+  height: 100.8px;
+  border-radius: 50px;
+  display: grid;
+  grid-auto-flow: row;
+  gap: 10px;
+  transform: scale(0.9);
+  transition: 0.3s;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  outline: none;
 }
-.category:hover{
-    content: "";
-    transform: scale(1.04);
-    transition: .3s;
+.category:hover {
+  transform: scale(1.04);
+  transition: 0.3s;
 }
-.category img{
-    height: 70px;
-    margin: auto;
+.category:focus {
+  transform: scale(1.04);
+  transition: 0.3s;
 }
-.category h3{
-    color: var(--white);
-    font-size: 18px;
-    text-align: center;
-    margin: 0;
+.category img {
+  height: 70px;
+  margin: auto;
 }
-  @media (max-width: 520px){
-    .category{
-      min-width: calc(25vw - 20px);  
-    }
+.category h3 {
+  color: var(--white);
+  font-size: 18px;
+  text-align: center;
+  margin: 0;
+}
+@media (max-width: 520px) {
+  .category {
+    min-width: calc(25vw - 20px);
   }
+}
 @media (max-width: 820px) {
-    .category h3{
-        font-size: 14px;
-        margin: 0;
-    }
+  .category h3 {
+    font-size: 14px;
+    margin: 0;
+  }
 }
 </style>
