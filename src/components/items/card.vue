84fb553f5261b14b24a425c8489db842c7c5f665
Fabian Pacherres <fabianpacherres@gmail.com>
2021-08-27 02:15:13 -0500

    Initial structure of the card

--- src/components/items/card.vue
+++ src/components/items/card.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="card" :style="`background-image: url(${url});`">
-    hola
+    <div class="card__container">
+      <div class="price">
+        $12.00
+      </div>
+      <div class="mainData">
+        Name foods
+      </div>
+    </div>
   </div>
 </template>
 
@@ -21,17 +28,45 @@ export default {
 <style scoped>
 @import "../../assets/styles/variables.css";
 .card {
-  background: blue;
   height: 300px;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
   border-radius: 15px;
+  transition: 0.5s;
+  border-bottom: 15px solid var(--bg-primary);
+}
+.card:hover {
+  transition: 0.5s;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+}
+.card__container {
+  height: 100%;
+  transition: 0.5s;
+  background-color: transparent;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  padding: 20px;
+  box-sizing: border-box;
+}
+.card__container:hover {
+  transform: scale(1);
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 15px;
+}
+.price{
+  margin-left: auto;
+  background-color: var(--bg-primary);
+  width: 80px;
+  padding: 5px;
+  text-align: center;
+  border-radius: 40px;
+  color: var(--white);
 }
 @media (max-width: 820px) {
   .card {
     height: 200px;
-    border-radius: 20px;
   }
 }
 </style>
