70d17cf911bc1ccbf2183ba3152c7d489c709c4b
Fabian Pacherres <fabianpacherres@gmail.com>
2021-08-29 16:07:32 -0500

    Modulo of Foods

---
+++ src/modules/foods.js
@@ -0,0 +1,11 @@
+import data from "../../api/data.json";
+
+export default({
+    namespaced: true,
+    state:{
+        foods: data.foods
+    },
+    mutations:{},
+    actions:{},
+    getters:{}
+})
