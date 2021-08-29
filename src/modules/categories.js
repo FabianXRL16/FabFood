c58e3357da692098fbf3e7b70ecb65fd8248011e
Fabian Pacherres <fabianpacherres@gmail.com>
2021-08-29 16:07:22 -0500

    Module Categories

---
+++ src/modules/categories.js
@@ -0,0 +1,11 @@
+import data from "../../api/data.json";
+
+export default({
+    namespaced: true,
+    state:{
+        categories: data.categories
+    },
+    mutations:{},
+    actions:{},
+    getters:{}
+})
