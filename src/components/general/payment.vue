55987d1289490b0634019806c4bcbc537b4b0ff8
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-02 23:16:45 -0500

    Component Payment creation

---
+++ src/components/general/payment.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container__payment">
+    Payment
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Payment",
+  components: {},
+};
+</script>
+<style scoped>
+@import "../../assets/styles/variables.css";
+.container__payment {
+  background: #00BF44;
+  box-sizing: border-box;
+  width: 400px;
+}
+@media (max-width: 1150px) {
+  .container__payment {
+      width: 100%;
+  }
+}
+</style>
