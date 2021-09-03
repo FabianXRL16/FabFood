b7f8e8fe1d0c926a50757edad04d74d62823ee74
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-02 23:18:48 -0500

    Shopping cart page with its components

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -1,5 +1,44 @@
 <template>
-    <div>
-        shopping cart
-    </div>
+  <div class="container">
+      <order-details>
+          carts
+      </order-details>
+      <payment>
+          payment
+      </payment>
+  </div>
 </template>
+
+<script>
+import orderDetails from "../components/groups/orderDetails.vue";
+import payment from "../components/general/payment.vue";
+export default {
+  name: "ShoppingCart",
+  components: {orderDetails, payment},
+  data(){
+      return{
+
+      }
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  padding: var(--p-main);
+  box-sizing: border-box;
+  display: flex;
+  gap: 20px;
+  justify-content: space-between;
+}
+@media (max-width: 1150px){
+    .container{
+        flex-direction: column;
+    }
+}
+@media (max-width: 820px) {
+  .container {
+    padding: var(--p-main-mobile);
+  }
+}
+</style>
