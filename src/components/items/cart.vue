7ec054fd830aced803b1e7a56af5bc5d41b18e0c
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-06 23:40:10 -0500

    Creation component of cart

---
+++ src/components/items/cart.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="img" style="background-image: url('https://barradeideas.com/wp-content/uploads/2019/09/fast-food.jpg')">
+
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    name: "Cart",
+    components: {
+
+    }
+}
+</script>
+
+<style scoped>
+.img{
+    height: 150px;
+    width: 180px;
+    border-radius: 10px;
+    background-position: center;
+    background-size: cover;
+}
+</style>
