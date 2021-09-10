fdef50f23f25e6175662e493ffef0801e544ba6b
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-09 20:15:59 -0500

    Creation component avatar in cart

---
+++ src/components/items/avatarCart.vue
@@ -0,0 +1,26 @@
+<template>
+    <div
+      class="img"
+      :style='`background-image:url(${img});`'
+    ></div>
+</template>
+<script>
+export default {
+  name: "AvatarCart",
+  components: {},
+  data(){
+    return{
+      img: 'https://barradeideas.com/wp-content/uploads/2019/09/fast-food.jpg'
+    }
+  }
+};
+</script>
+<style scoped>
+.img{
+    height: 60px;
+    width: 60px;
+    border-radius: 15px;
+    background-position: center;
+    background-size: cover;
+}
+</style>
