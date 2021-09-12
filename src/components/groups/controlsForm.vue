d84762687b447fb9b832d655a37b7349662a3a4a
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-12 01:51:29 -0500

    Creation of controls in the form

---
+++ src/components/groups/controlsForm.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="controlsForm">
+    <button class="btn btnReset" type="reset">
+      <i class="fas fa-redo-alt"></i>
+    </button>
+    <input class="btn stepNext" type="submit" :value="text" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "controlsForm",
+  props: {
+    text: {
+      type: String,
+      default: "Next",
+    },
+  },
+  components: {},
+};
+</script>
+
+<style scoped>
+.controlsForm {
+  display: grid;
+  grid-template-columns: 50px 1fr;
+  gap: 20px;
+  margin-top: 20px;
+}
+.btnReset {
+  border-radius: 5px;
+  background-color: #00aeff;
+}
+.btnReset i {
+  color: var(--white);
+  font-size: 20px;
+}
+</style>
