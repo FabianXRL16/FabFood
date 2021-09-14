2d5bb4677f32fb99f50f7ce3bbeb431ed6fd5e79
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-14 02:32:51 -0500

    Change of height in container cart

--- src/components/groups/orderDetails.vue
+++ src/components/groups/orderDetails.vue
@@ -1,40 +1,38 @@
 <template>
   <div class="container__orderDetails">
-    <back class="title_back" :title="'Shopping Cart'" />
     <div class="container__cart">
       <cart v-for="n in 10" :key="n" />
     </div>
   </div>
 </template>
 <script>
-import back from "../general/back.vue";
 import cart from "../items/cart.vue";
 export default {
   name: "OrderDetails",
   components: {
-    cart, back
+    cart,
   },
 };
 </script>
 
 <style scoped>
 @import "../../assets/styles/variables.css";
-.title_back{
-  margin: 20px 0;
-}
 .container__cart{
-  height: calc(100vh - 70px - 99px);
+  height: calc(100vh - 70px - 110px);
   box-sizing: border-box;
   overflow-y: auto;
-  padding-bottom: 10px;
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 9.5px;
 }
 .container__cart::-webkit-scrollbar{
   width: 0;
 }
 @media (max-width: 1160px){
   .container__cart{
-    height: calc(100vh - 70px - 172px);
+    height: calc(100vh - 70px - 180px);
     padding-bottom: 0;
+    gap: 13px;
   }
 }
 </style>
