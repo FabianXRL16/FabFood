85e343c94682dd7c6841100c19cd61d90fc0fda6
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-14 23:09:59 -0500

    Component structure refactoring of page ShoppingCart

--- src/views/ShoppingCart.vue
+++ src/views/ShoppingCart.vue
@@ -1,54 +1,83 @@
 <template>
-  <div class="container__shopping">
-    <order-details v-if="mobileTitle" />
-    <payment class="payment" />
-    <payment v-if="!mobileTitle" @noToShow="mobileTitle = !mobileTitle" />
+  <div class="container__shoppingCart">
+    <back
+      v-if="paymentMobile"
+      :title="'Shopping Cart'"
+      class="container__back"
+    />
+    <order-details v-if="paymentMobile" class="container__order" />
+    <payment
+      :class="paymentMobile ? 'container__payment' : 'container__payment-p '"
+    />
     <title-payment-mobile
-      v-if="mobileTitle"
-      class="smallPayment"
-      :totalPrice="212.0"
-      @toShow="mobileTitle = !mobileTitle"
+      class="container__title-payment-mobile"
+      @show="paymentMobile = !paymentMobile"
     />
   </div>
 </template>
 
 <script>
-import orderDetails from "../components/groups/orderDetails.vue";
 import payment from "../components/general/payment.vue";
+import orderDetails from "../components/groups/orderDetails.vue";
 import TitlePaymentMobile from "../components/general/titlePaymentMobile.vue";
+import back from "@/components/general/back.vue";
 export default {
   name: "ShoppingCart",
   components: {
-    orderDetails,
+    back,
     payment,
+    orderDetails,
     TitlePaymentMobile,
   },
   data() {
     return {
-      mobileTitle: true,
+      paymentMobile: true,
     };
   },
 };
 </script>
 
 <style scoped>
-.container__shopping {
+.container__shoppingCart {
+  color: blanchedalmond;
+  padding-top: 20px;
+  box-sizing: border-box;
   display: grid;
+  grid-template-areas:
+    "back back"
+    "order payment";
   grid-template-columns: 1fr 450px;
+  grid-template-rows: 50px 1fr;
   gap: 20px;
 }
-.smallPayment {
+.container__back {
+  grid-area: back;
+}
+.container__order {
+  grid-area: order;
+}
+.container__payment {
+  grid-area: payment;
+}
+.container__title-payment-mobile {
+  grid-area: title;
   display: none;
 }
 @media (max-width: 1160px) {
-  .container__shopping {
-    grid-template-columns: 1fr;
-    gap: 0;
+  .container__shoppingCart {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    padding-top: 20px;
+    padding-bottom: 20px;
   }
-  .payment {
+  .container__title-payment-mobile {
+    display: flex;
+  }
+  .container__payment {
     display: none;
   }
-  .smallPayment {
+  .container__payment-p {
     display: flex;
   }
 }
