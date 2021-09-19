1f012d85c812221477d1048c86f22ba9a6e552da
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-19 03:19:36 -0500

    Add style a content details in listFoodsCombo

--- src/components/general/contentCombos.vue
+++ src/components/general/contentCombos.vue
@@ -32,10 +32,17 @@ export default {
   color: antiquewhite;
   display: grid;
   grid-template-columns: 1fr 400px;
+  background-color: rgba(242, 7, 70, 0.1);
+  border-radius: 5px;
+  padding: 30px;
+  margin-top: 40px;
+  gap: 50px;
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
 }
 @media (max-width: 1180px) {
   .details {
     grid-template-columns: 1fr;
+    gap: 20px;
   }
 }
 </style>
