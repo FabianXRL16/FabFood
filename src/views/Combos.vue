c51568e02093bdd29177486710f13ca518d84624
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-18 23:44:46 -0500

    Page Combos creation

---
+++ src/views/Combos.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container__combos">
+    <back :title="'Combos'" />
+    <content-combos />
+  </div>
+</template>
+
+<script>
+import back from "../components/general/back";
+import contentCombos from "../components/general/contentCombos";
+export default {
+  name: "Combos",
+  components: {
+    back,
+    contentCombos,
+  },
+};
+</script>
+
+<style scoped>
+.container__combos{
+    padding-top: 20px;
+    padding-bottom: 20px;
+    display: grid;
+    grid-template-rows: auto 1fr;
+    gap: 20px;
+    overflow-x: auto;
+}
+.container__combos::-webkit-scrollbar{
+  width: 0;
+  
+}
+</style>
