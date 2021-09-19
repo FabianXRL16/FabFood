8f751b229d72f617d6af407865c267eb6d66badd
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-18 23:45:58 -0500

    Controls panner created

---
+++ src/components/groups/buttonsPanner.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="containerButtonPanner">
+    <button class="btn btnPanner" v-for="n in 3" :key="n"></button>
+  </div>
+</template>
+<script>
+export default {
+  name: "buttonsPanner",
+};
+</script>
+<style scoped>
+.containerButtonPanner {
+  height: 40px;
+  display: flex;
+  gap: 30px;
+  justify-content: center;
+  align-items: flex-end;
+}
+.btnPanner {
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  background-color: var(--bg-secondary);
+  transition: .3s;
+}
+.btnPanner:nth-child(1) {
+  background-color: var(--bg-primary);
+}
+.btnPanner:hover {
+  background-color: var(--bg-primary);
+  transition: .3s;
+}
+</style>
