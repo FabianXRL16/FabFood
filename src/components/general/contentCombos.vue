16ef6ea9204e672e23f457c501931d6b1e7043a9
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-18 23:45:04 -0500

    Created container of combos

---
+++ src/components/general/contentCombos.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="panner">
+      <panner-combo />
+      <buttons-panner />
+    </div>
+    <div class="details">
+      <list-foods-combo />
+      <order-combo />
+    </div>
+  </div>
+</template>
+
+<script>
+import PannerCombo from "./pannerCombo.vue";
+import listFoodsCombo from "./listFoodsCombo.vue";
+import OrderCombo from "./orderCombo.vue";
+import buttonsPanner from "../groups/buttonsPanner.vue";
+export default {
+  name: "contentCombos",
+  components: {
+    PannerCombo,
+    listFoodsCombo,
+    OrderCombo,
+    buttonsPanner,
+  },
+};
+</script>
+
+<style scoped>
+.details {
+  color: antiquewhite;
+  display: grid;
+  grid-template-columns: 1fr 400px;
+}
+@media (max-width: 1180px) {
+  .details {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
