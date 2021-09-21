9d84a14d2cce72c08923da6461d6854018945a0e
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-20 23:41:50 -0500

    Container principles in home

---
+++ src/components/general/containerInfoPrinciples.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="containerInfo">
+    <div class="contentInfo">
+      <card-home v-for="item in dataCard" :key="item.id" :data="item">
+        <template>
+          <img
+            class="iconPrinciples"
+            :src="require(`../../assets/img/${item.name.toLowerCase()}.svg`)"
+            alt=""
+          />
+        </template>
+      </card-home>
+    </div>
+  </div>
+</template>
+
+<script>
+import cardHome from "../items/cardHome.vue";
+export default {
+  components: { cardHome },
+  name: "containerInfoPrinciples",
+  data() {
+    return {
+      dataCard: [
+        {
+          id: 0,
+          name: "Fast",
+          description: "Lorem  ads dasd as d a sf erqquqt rnu w56 i 56in 75 ",
+          icon: "@/assets/img/fast.svg",
+        },
+        {
+          id: 1,
+          name: "Delicious",
+          description: "Lorem  ads dasd as d a sf erqquqt rnu w56 i 56in 75 ",
+          icon: "@/assets/img/delicious.svg",
+        },
+        {
+          id: 2,
+          name: "Health",
+          description: "Lorem  ads dasd as d a sf erqquqt rnu w56 i 56in 75 ",
+          icon: "@/assets/img/Health.svg",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.containerInfo {
+  position: absolute;
+  width: 100%;
+  left: 0;
+  background: var(--bg-primary);
+  margin: 10px 0 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 60px var(--p);
+  box-sizing: border-box;
+  color: var(--white);
+}
+.contentInfo {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+  height: auto;
+  gap: 30px 50px;
+}
+.iconPrinciples {
+  height: 50px;
+}
+@media (max-width: 1200px) {
+  .contentInfo {
+    gap: 10px 40px;
+  }
+}
+@media (max-width: 820px) {
+  .containerInfo {
+    padding: 40px var(--p-mobile);
+    margin: 40px 0 10px;
+  }
+}
+</style>
