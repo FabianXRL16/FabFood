1f9a4f2c24e9059b9b76c38f939d2f4acf7d1fce
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-20 23:42:43 -0500

    Created card of home

---
+++ src/components/items/cardHome.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="cardHome">
+    <slot />
+    <h2 class="principles">{{ data.name }}</h2>
+    <p>
+      {{ data.description }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cardHome",
+  props: {
+    data: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style scoped>
+.cardHome {
+  width: 300px;
+  color: var(--white);
+  text-align: center;
+}
+.principles {
+  margin: 0;
+}
+</style>
