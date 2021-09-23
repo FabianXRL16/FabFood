a7d4da12a4a1dd1aa15519cfbfbc4c6dcd1c4273
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-22 22:41:07 -0500

    Created footer

---
+++ src/components/general/containerFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer class="containerFooter">
+    footerrr
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "containerFooter",
+};
+</script>
+
+<style scoped>
+.containerFooter {
+  background-color: var(--black);
+  color: brown;
+  padding: 0 var(--p);
+}
+@media (max-width: 820px) {
+  .containerFooter {
+    padding: 0 var(--p-mobile);
+  }
+}
+</style>
