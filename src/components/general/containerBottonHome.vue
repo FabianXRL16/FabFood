6d869e3bb7ab7707537df9b70d4f4144f838d151
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-22 22:40:46 -0500

    Container main botton of home

---
+++ src/components/general/containerBottonHome.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="containerBottonHome">
+    <div class="containerBotton">
+      <container-chef />
+      <container-footer />
+    </div>
+  </div>
+</template>
+
+<script>
+import containerChef from "./containerChef.vue";
+import ContainerFooter from "./containerFooter.vue";
+export default {
+  components: { containerChef, ContainerFooter },
+  name: "containerBottonHome",
+};
+</script>
+
+<style scoped>
+.containerBotton {
+  position: absolute;
+  display: grid;
+  grid-template-rows: repeat(2, auto);
+  width: 100vw;
+  left: 0;
+}
+</style>
