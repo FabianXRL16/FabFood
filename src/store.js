54dd9325e6f185ef5c5cfe7a23f794a4d74265ae
Fabian Pacherres <fabianpacherres@gmail.com>
2021-09-24 23:13:41 -0500

    Created order en store

--- src/store.js
+++ src/store.js
@@ -1,22 +1,33 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-import foods from './modules/foods.js'
-import categories from './modules/categories.js'
+import foods from "./modules/foods.js";
+import categories from "./modules/categories.js";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+export const state = () => ({
+  _order: [],
+});
 
 export default new Vuex.Store({
-    state:{
-
+  state: {
+    getOrder(state) {
+      return state._order;
     },
-    mutations:{
-
+  },
+  ations: {
+    registerOrder({ commit }, order) {
+      commit("ADD_FOOD_TO_ORDER", order);
     },
-    ations:{
-
+  },
+  mutations: {
+    ADD_FOOD_TO_ORDER(state, newOrder) {
+      state._order.push(newOrder);
     },
-    modules:{
-        foods, categories
-    }
-})
+  },
+  modules: {
+    foods,
+    categories,
+  },
+});
