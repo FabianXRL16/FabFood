1173af4dd9cd838a5e0f9129934d7a469bfdfd7c
FabianXRL16 <fabianpacherres@gmail.com>
2022-06-21 14:03:30 -0500

    add orders in carts

--- src/components/groups/orderDetails.vue
+++ src/components/groups/orderDetails.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container__orderDetails">
     <div class="container__cart">
-      <cart v-for="n in 10" :key="n" />
+      <cart v-for="order in orders" :key="order.id" :order="order"/>
     </div>
   </div>
 </template>
@@ -12,6 +12,11 @@ export default {
   components: {
     cart,
   },
+  computed:{
+    orders() {
+      return this.$store.state.__order
+    }
+  }
 };
 </script>
 
@@ -21,8 +26,8 @@ export default {
   height: calc(100vh - 70px - 112px);
   box-sizing: border-box;
   overflow-y: auto;
-  display: grid;
-  grid-template-columns: 1fr;
+  display: flex;
+  flex-direction: column;
   gap: 9.2px;
 }
 .container__cart::-webkit-scrollbar{
