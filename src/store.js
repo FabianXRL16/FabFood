871118ac0e7dd2b4c33be4dc7aec59c34c9b838f
FabianXRL16 <fabianpacherres@gmail.com>
2022-06-21 20:10:08 -0500

    reset count in foods

--- src/store.js
+++ src/store.js
@@ -1,46 +1,54 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-import data from "../api/data.json";
+import data from '../api/data.json'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
     foods: data.foods,
     categories: data.categories,
     __order: [],
-    __countOrder: 0
+    __countOrder: 0,
   },
   getters: {
     newOrder: (state) => state.__order,
-    updateCountOrder: (state) => state.__countOrder
+    updateCountOrder: (state) => state.__countOrder,
   },
   actions: {
     addOrder({ commit }, newOrder) {
-      commit("ADD_ORDER", newOrder);
+      commit('ADD_ORDER', newOrder)
     },
     updateCountOrder({ commit }) {
-      commit("UPDATED_COUNT_ORDER");
+      commit('UPDATED_COUNT_ORDER')
     },
-    deleteOrder({commit}, id) {
-      commit("DELETE_ORDER", id)
+    deleteOrder({ commit }, id) {
+      commit('DELETE_ORDER', id)
+    },
+    orderCountFood({ commit }, obj) {
+      commit('ORDER_COUNT_FOOD', obj)
     }
   },
   mutations: {
     ADD_ORDER(state, newOrder) {
       state.__order.push(newOrder)
-      let pos = state.foods.findIndex(i => i.id === newOrder.id)
+      let pos = state.foods.findIndex((i) => i.id === newOrder.id)
       state.foods[pos].order = true
     },
     UPDATED_COUNT_ORDER(state) {
       state.__countOrder = state.__order.length
     },
-    DELETE_ORDER(state, id){
-      let pos = state.__order.findIndex(i => i.id === id)
-      let posParent = state.foods.findIndex(i => i.id === id)
+    DELETE_ORDER(state, id) {
+      let pos = state.__order.findIndex((i) => i.id === id)
+      let posParent = state.foods.findIndex((i) => i.id === id)
       state.__order.splice(pos, 1)
       state.foods[posParent].order = false
+      state.foods[posParent].count = 1
+    },
+    ORDER_COUNT_FOOD(state, obj) {
+      let pos = state.__order.findIndex((i) => i.id === obj.id)
+      state.__order[pos].count = state.__order[pos].count + obj.counter
     }
-  }
-});
+  },
+})
