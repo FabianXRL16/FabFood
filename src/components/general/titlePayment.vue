71c3b557ac850f4bf6c114fd0d005d0f1acf483e
FabianXRL16 <fabianpacherres@gmail.com>
2022-06-21 23:17:16 -0500

    total price in title payment desktop

--- src/components/general/titlePayment.vue
+++ src/components/general/titlePayment.vue
@@ -11,11 +11,16 @@
 <script>
 export default {
   name: "titlePayment",
-  props: {
-    totalPrice: {
-      type: Number,
-      default: 0.0,
-    },
+  computed: {
+    totalPrice() {
+      let order = this.$store.state.__order
+      let price = 0
+      order.map((i) => {
+        let count = i.count * i.price
+        price = price + count
+      })
+      return price
+    }
   },
   methods:{
     closed(){
